663d3fb9c3ad5ce8a7deebb83a7451e1a113e6cf
trelea <treleamarius76@gmail.com>
2024-11-10 23:38:57 +0200

    cors

--- src/config/cors.config.ts
+++ src/config/cors.config.ts
@@ -1,4 +1,11 @@
 export const corsConf = {
-    origin: ['http://192.168.1.8:2000', 'http://localhost:5173', 'http://localhost:2005', 'https://localhost'],
-    credentials: true
+  origin: [
+    'http://192.168.1.8:2000',
+    'http://localhost:5173',
+    'http://localhost:2005',
+    'https://localhost',
+    process.env.CORS_DEV,
+    process.env.CORS_PROD,
+  ],
+  credentials: true,
 };
